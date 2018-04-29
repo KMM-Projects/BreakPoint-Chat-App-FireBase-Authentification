b6b3ec9b08cb9782354a5d484a5a2dfd42f56ee7
Patrik Kemeny <patrikkemeny@Patriks-MacBook-Pro.local>
2018-04-29 15:27:10 +1000

    v3

--- breakpoint/Controller/CreateGroupsVC.swift
+++ breakpoint/Controller/CreateGroupsVC.swift
@@ -20,6 +20,8 @@ class CreateGroupsVC: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.delegate = self
+        tableView.dataSource = self
 
        
     
@@ -31,3 +33,28 @@ class CreateGroupsVC: UIViewController {
     @IBAction func closeBtnWasPressed(_ sender: Any) {
     }
 }
+
+extension CreateGroupsVC: UITableViewDelegate, UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 2
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "userCell") as? UserCell  else {
+            return UITableViewCell() }
+       let profileImage = UIImage(named: "defaultProfileImage")
+        cell.configureCell(profileImage: profileImage!, email: "random@email.com", isSelected: true)
+        return cell
+    }
+    
+}
+
+
+
+
+
+
+
---
+++ breakpoint/View/UserCell.swift
@@ -0,0 +1,35 @@
+//
+//  UserCell.swift
+//  breakpoint
+//
+//  Created by Patrik Kemeny on 29/4/18.
+//  Copyright © 2018 Caleb Stultz. All rights reserved.
+//
+
+import UIKit
+
+class UserCell: UITableViewCell {
+
+    @IBOutlet weak var profileImage: UIImageView!
+    @IBOutlet weak var emailLabel: UILabel!
+    @IBOutlet weak var checkImage: UIImageView!
+    
+    func configureCell(profileImage image: UIImage, email: String, isSelected: Bool){
+        self.profileImage.image = image
+        self.emailLabel.text = email
+        
+        if isSelected  {
+            self.checkImage.isHidden = false
+        } else {
+            self.checkImage.isHidden = true
+        }
+    }
+    
+// to toggled the checkMark
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
